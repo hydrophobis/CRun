d7cccb2b372f460d0628f27188a20a6a87fef3ad
hydrophobis <kasenmensing@gmail.com>
2025-01-13 18:11:34 -0600

    Added file selection and removed debug messages

--- crun.cc
+++ crun.cc
@@ -5,24 +5,33 @@
 #include <cstring>
 
 int main(int argc, char* argv[]){
+	std::string outfilename = "";
+	for(int i = 0; i < argc; i++){
+		if(argv[i] == "-o"){
+			outfilename = argv[i + 1];
+			i += 1;
+		}
+	}
+	if(outfilename == ""){
+		outfilename = argv[1];
+	}
+
 	char tmp[106] = "/tmp/";
 	char fileExtension[6] = ".crun";
-	char* inFile = argv[1];
-	std::strcat(tmp, inFile);
+	char** inFile = argv;
+	std::string files = "";
+	for(int i = 0; i < sizeof(inFile)/8; i++){
+		std::string s(inFile[i + 1]);
+		files = files + s + " ";
+	}
+	std::strcat(tmp, outfilename.c_str());
 	std::strcat(tmp, fileExtension);
 	std::strcat(tmp, " ");
-	std::strcat(tmp, inFile);
-	
-	for(int i = 1; i < argc; i++){
-		
-	}
+	std::strcat(tmp, files.c_str());
 
 	char compileCommand[100] = "gcc -o ";
 
 	std::strcat(compileCommand, tmp);
-	char buffer[121] = "";
-	std::strcat(buffer, compileCommand);			
-	system(buffer);
 	std::cout << "Starting child process" << std::endl;
 	system(tmp);
 	std::cout << std::endl << "Child process finished." << std::endl;
